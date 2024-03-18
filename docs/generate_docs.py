7fcecb2c3d67e710892659e3ae8890152a9ab5ee
SteGala <galaxx.ste@gmail.com>
2024-03-18 20:48:24 +0100

    update README generation

--- docs/generate_docs.py
+++ docs/generate_docs.py
@@ -17,12 +17,20 @@ def generate_html(data, depth, res):
             generate_html(data[key]["properties"], depth + 1, res)
 
 dir = "../models/schemas"
-example_dir = "../models/examples/svg"
+example_dir = "models/examples/svg"
 
 filenames = ["flavor.json", "flavor-types/k8slice.json", "flavor-types/service.json", "flavor-types/vm.json", "flavor-types/sensor.json"]
 res = [""]
+res.append("```")
+res.append("DISCLAIMER #1: The content of this document is automatically generated upon pushing the files on GitHub. The generation starts from the JSON schema and examples contained in the repository. Do not try to modify this document, just the JSON files.")
+res.append("```")
+res.append("```")
+res.append("DISCLAIMER #2: Still, the generation of examples of JSONs (starting from the schema) is not automated yet. Therefore, we suggest to use tools like ChatGPT or Gemini, providing the JSON schema to the prompt and asking for an example of compliant JSON.")
+res.append("```")
+res.append("In the following we represent some examples of JSON, you can find the original JSON schemas [here](models/schemas).")
 
 for id, f_name in enumerate(filenames):
+    res.append("")
     if f_name == "flavor.json":
         res.append("# Flavor")
     else:
@@ -36,10 +44,11 @@ for id, f_name in enumerate(filenames):
     generate_html(data["properties"], 0, res)
     
     if f_name == "flavor.json":
+        res.append("")
         res.append("# FlavorType")
         res.append("The FlavorType describes the actual flavor that is adverised.")
         
-text_file = open("README.md", "w")
+text_file = open("../README.md", "w")
  
 #write string to file
 text_file.write("\n".join(res))
